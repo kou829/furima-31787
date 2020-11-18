4f6c6c3b952ce7f7f5e1db942217cabff2375256
kou829 <73116796+kou829@users.noreply.github.com>
2020-11-18 15:01:26 +0900

    ユーザ管理機能の実装・テストコード２

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
-  validates :birth_date, presence: true
+  with_options presence: true do
+    validates :nickname
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, {format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }}
+    VALID_PASSWORD_REGEX =/\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[\d])\w{6,12}\z/
+    validates :password,format: { with: VALID_PASSWORD_REGEX }
+    with_options format: {with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/} do
+      validates :last_name
+      validates :first_name
+    end
+    with_options format: {with: /\A[ァ-ヶー－]+\z/} do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+    validates :birth_date
+  end 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,44 +4,52 @@ RSpec.describe User, type: :model do
   describe "ユーザ新規登録" do
 
     it "nicknameが空だと登録できない" do
-      user = User.new(nickname: "", email: "furi@com", password: "furi123", password_confirmation: "furi123", last_name: "田中", first_name: "良", last_name_kana: "タナカ", first_name_kana: "リョウ", birth_date: "1998-05-07")
+      user = FactoryBot.build(:user)  # Userのインスタンス生成
+      user.nickname = "" # nicknameの値を空にする
       user.valid?
-      expect(user.errors.full_messages).to include("Nickname can't be blank")
+      expect(user.errors.full_messages).to include "Nickname can't be blank"
     end
     it "emailが空だと登録できない" do
-      user = User.new(nickname: "furima太郎", email: "", password: "furi123", password_confirmation: "furi123", last_name: "田中", first_name: "良", last_name_kana: "タナカ", first_name_kana: "リョウ", birth_date: "1998-05-07")
+      user = FactoryBot.build(:user)  
+      user.email = "" 
       user.valid?
-      expect(user.errors.full_messages).to include("Email can't be blank")
+      expect(user.errors.full_messages).to include "Email can't be blank"
     end
     it "passwordが空だと登録できない" do
-      user = User.new(nickname: "furima太郎", email: "furi@com", password: "", password_confirmation: "", last_name: "田中", first_name: "良", last_name_kana: "タナカ", first_name_kana: "リョウ", birth_date: "1998-05-07")
+      user = FactoryBot.build(:user)  
+      user.password = ""
       user.valid?
-      expect(user.errors.full_messages).to include("Password can't be blank")
+      expect(user.errors.full_messages).to include "Password can't be blank"
     end
     it "last_nameが空だと登録できない" do
-      user = User.new(nickname: "furima太郎", email: "furi@com", password: "furi123", password_confirmation: "furi123", last_name: "", first_name: "良", last_name_kana: "タナカ", first_name_kana: "リョウ", birth_date: "1998-05-07")
+      user = FactoryBot.build(:user)
+      user.last_name= ""
       user.valid?
-      expect(user.errors.full_messages).to include("Last name can't be blank")
+      expect(user.errors.full_messages).to include "Last name can't be blank"
     end
     it "first_nameが空だと登録できない" do
-      user = User.new(nickname: "furima太郎", email: "furi@com", password: "furi123", password_confirmation: "furi123", last_name: "田中", first_name: "", last_name_kana: "タナカ", first_name_kana: "リョウ", birth_date: "1998-05-07")
+      user = FactoryBot.build(:user)
+      user.first_name = ""
       user.valid?
-      expect(user.errors.full_messages).to include("First name can't be blank")
+      expect(user.errors.full_messages).to include "First name can't be blank"
     end
     it "last_name_kanaが空だと登録できない" do
-      user = User.new(nickname: "furima太郎", email: "furi@com", password: "furi123", password_confirmation: "furi123", last_name: "田中", first_name: "良", last_name_kana: "", first_name_kana: "リョウ", birth_date: "1998-05-07")
+      user = FactoryBot.build(:user)
+      user.last_name_kana = ""
       user.valid?
-      expect(user.errors.full_messages).to include("Last name kana can't be blank")
+      expect(user.errors.full_messages).to include "Last name kana can't be blank"
     end
     it "first_name_kanaが空だと登録できない" do
-      user = User.new(nickname: "furima太郎", email: "furi@com", password: "furi123", password_confirmation: "furi123", last_name: "田中", first_name: "良", last_name_kana: "タナカ", first_name_kana: "", birth_date: "1998-05-07")
+      user = FactoryBot.build(:user)
+      user.first_name_kana = ""
       user.valid?
-      expect(user.errors.full_messages).to include("First name kana can't be blank")
+      expect(user.errors.full_messages).to include "First name kana can't be blank"
     end
     it "birth_dateが空だと登録できない" do
-      user = User.new(nickname: "furima太郎", email: "furi@com", password: "furi123", password_confirmation: "furi123", last_name: "田中", first_name: "良", last_name_kana: "タナカ", first_name_kana: "リョウ", birth_date: "")
+      user = FactoryBot.build(:user)
+      user.birth_date = ""
       user.valid?
-      expect(user.errors.full_messages).to include("Birth date can't be blank")
+      expect(user.errors.full_messages).to include "Birth date can't be blank"
     end
 
   end
