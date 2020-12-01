0aa4fffb7ff1754154b7d61cc34df8e58f0da777
kou829 <73116796+kou829@users.noreply.github.com>
2020-12-01 11:40:21 +0900

    アプリ名の修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.14.1'
 
 # Capistranoのログの表示に利用する
-set :application, 'furima31787'
+set :application, 'furima-31787'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:kou829/furima-31787.git'
